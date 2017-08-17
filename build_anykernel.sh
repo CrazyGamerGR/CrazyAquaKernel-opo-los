1fff51f6b47d02bd4d2436a2c77963afbc8dbeda
CrazyGamerGR <frankalexb97@gmail.com>
2017-08-17 23:33:07 +0300

    update build_anykernel.sh

--- build_anykernel.sh
+++ build_anykernel.sh
@@ -12,7 +12,7 @@ export CROSS_COMPILE=/home/francesco/arm-linux-androideabi-4.9/bin/arm-linux-and
 ##########################################
 mkdir output
 ##########################################
-make -C $(pwd) O=output cyanogenmod_bacon_defconfig 
+make -C $(pwd) O=output lineageos_bacon_defconfig 
 make -C $(pwd) O=output
 ##########################################
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
